430fbbab294df2db94af5ddb353b37c0a4100bc4
krishna-keshab-banik <kb8234@srmist.edu.in>
2026-02-19 08:52:47 +0530

    [Neelansh Goyal , Completed , UC1]

---
+++ palindrome/src/Palindrome.java
@@ -0,0 +1,8 @@
+public class Palindrome
+{
+    static void main() {
+        System.out.println("Welcome to the Palindrome checker management system.");
+        System.out.println("Verion : 1.0");
+        System.out.println("System Initialized Successfully.");
+    }
+}
