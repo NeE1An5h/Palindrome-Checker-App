ef7582cd3734c790625864bff4f58833976296c9
NeE1An5h <neelanshgoyal@gmail.com>
2026-03-06 08:55:03 +0530

    UC10-Completed

--- palindrome/src/Palindrome.java
+++ palindrome/src/Palindrome.java
@@ -1,24 +1,28 @@
 import java.util.*;
 
 public class Palindrome {
-
-    static boolean check(String s, int start, int end) {
-        if (start >= end)
-            return true;
-
-        if (s.charAt(start) != s.charAt(end))
-            return false;
-
-        return check(s, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        if (check(input, 0, input.length() - 1))
+        String s = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = s.length() - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
